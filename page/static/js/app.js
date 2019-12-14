9c14748092eb08e28b9e2d344659172986e9d700
curtis chat <curtis.ch.chat@gmail.com>
2019-12-13 17:56:06 -0700

    ourWork grids

--- page/static/js/app.js
+++ page/static/js/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
 
+    // Initiate animate on scroll
+	AOS.init();
+
     // Hamburger open close toggle top nav
     $('.hamburger-toggle').on('click', function () {
         $(this).toggleClass('x-toggle');
         $('nav').toggleClass('res-menu');
     });
+
+    // Initiate smooth scroll
+    $('a').smoothScroll({
+		speed: 1000
+	});
     
 });
 
